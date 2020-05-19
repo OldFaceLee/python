c32ca176a77b180f18b2845ce4b7bac2fe1b0708
lixuejun <lixj5@asiainfo.com>
2020-05-19 09:56:17 +0800

    优化xlrd读取excel

--- venv/com_ai_util/excel_util.py
+++ venv/com_ai_util/excel_util.py
@@ -10,7 +10,18 @@ import xlrd
 
 class ExcelUtil:
 
-    def read(self, file_path, sheet_name):
+    def read_excel(self, file_path, sheet_name, row_no, column_no):
         workbook = xlrd.open_workbook(file_path)
         sheet = workbook.sheet_by_name(sheet_name)
-        
+        cell_data = sheet.cell(row_no, column_no)
+        if cell_data.ctype == 1:
+            print("返回的cell内的数据格式为 %d, 是str类型, 值为:%s" % (cell_data.ctype, cell_data))
+        elif cell_data.ctype == 2:
+            print("返回的cell内的数据格式为 %d, 是number类型, 值为:%s" % (cell_data.ctype, cell_data))
+        return cell_data
+
+
+instance_excel = ExcelUtil()
+path = "/Users/macos/Desktop/testParam.xlsx"
+cell = instance_excel.read_excel(path, "tc01", 0, 0)
+
